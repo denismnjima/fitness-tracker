7f1a118f7fac4a795472b36504bad1fe225f9540
denismnjima <denismnjima@gmail.com>
2024-08-17 14:39:06 +0300

    initial commit

---
+++ src/Components/Homepage.jsx
@@ -0,0 +1,13 @@
+import DashboardTabs from "./DashboardTabs"
+import Navbar from "./Navbar"
+
+function Homepage() {
+  return (
+    <div className="dashboard">
+        <Navbar />
+        <DashboardTabs/>
+    </div>
+  )
+}
+
+export default Homepage
