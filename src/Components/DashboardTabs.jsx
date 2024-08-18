0ea2df5d3e338df0b977b04fa0cd0f50f94dd1c4
denismnjima <denismnjima@gmail.com>
2024-08-18 13:51:10 +0300

    adds Distance tracker

--- src/Components/DashboardTabs.jsx
+++ src/Components/DashboardTabs.jsx
@@ -3,27 +3,39 @@ import { useEffect, useState } from "react";
 import { supabase } from "../config/supabaseClient";
 import { useUser } from "../Hooks/useUserStore";
 function DashboardTabs() {
-  // const [loading,setLoading] = useState(false)
-  // useEffect(() => {
-  //   const fetchActivities = async () => {
-  //     const today = new Date().toISOString().split('T')[0];
-  //     const { data, error } = await supabase
-  //       .from('user_activities')
-  //       .select('*')
-  //       .eq('user_id', user.id)
-  //       .like('activity_date', `${today}%`); 
+  const {user,setuser} = useUser((state)=>({
+    user:state.user,
+    setUser:state.setUser
+  }))
+  const [loading,setLoading] = useState(false)
+  useEffect(() => {
+    const userData = sessionStorage.getItem('user');
+    if (!userData) {
+      navigate('/login');
+    }else{
+      setUser(JSON.parse(userData))
+    }
+  }, [navigate]);
+  useEffect(() => {
+    const fetchActivities = async () => {
+      const today = new Date().toISOString().split('T')[0];
+      const { data, error } = await supabase
+        .from('user_activities')
+        .select('*')
+        .eq('user_id', user.id)
+        .like('activity_date', `${today}%`); 
 
-  //     if (error) {
-  //       console.error('Error fetching activities:', error);
-  //     } else {
-  //       console.log(data)
-  //     }
+      if (error) {
+        console.error('Error fetching activities:', error);
+      } else {
+        console.log(data)
+      }
 
-  //     // setLoading(false);
-  //   };
+      // setLoading(false);
+    };
 
-  //   fetchActivities();
-  // }, []);
+    fetchActivities();
+  }, []);
 
   return (
     <div className="dashboard-tabs">
